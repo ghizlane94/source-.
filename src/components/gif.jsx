a72ac43a842dd02f36bb64bbfcd8903d7795b72b
ghizlane ait dadda ali <ghizlane.aitdaddaali94@gmail.com>
2024-05-07 13:38:53 +0100

    first commit

---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Gif extends Component{
+  render(){
+    return(
+      <img src="https://media0.giphy.com/media/mxjzBpyu8DDLIcEKVC/200.gif?cid=790b7611htape06fa9ko4kzawq18t5712nn0quzjpwj2xeog&ep=v1_gifs_trending&rid=200.gif&ct=g" className='gif' />
+    )
+  }
+}
+
+export default Gif;
---
+++ src/components/gif_list.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Gif from './gif.jsx';
+class GifList extends Component{
+  render(){
+    return(
+      <div className='right-scene '>
+        <Gif/>
+      </div>
+    )
+  }
+}
+
+export default GifList;
